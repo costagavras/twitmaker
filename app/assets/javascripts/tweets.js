0be27c96d19ee4e4eda195d7cc9bce96a2ff263b
costagavras <cgavras@yahoo.ca>
2018-11-01 23:22:46 -0400

    AJAX request and refactoring Index view

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,17 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+document.addEventListener("DOMContentLoaded", function() {
+
+    var tweet_form = document.querySelector('#new_tweet');
+    var formData = new FormData(tweet_form);
+
+
+    form.addEventListener('submit',function(e){
+
+        e.preventDefault();
+        var formData = new FormData(form);
+        var send_tweet = axios({
+            url: form.getAttribute('action') ,
+            method: form.getAttribute('method'),
+            data: formData,
+        })
+
+});
